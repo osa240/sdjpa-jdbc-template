1f8546a83c4cffefdaafe7b8ece01bda97418e42
serhii.orlov <serhii.orlov@extrawest.com>
2022-10-24 18:04:31 +0300

    update user

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -36,7 +36,9 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        jdbcTemplate.update("UPDATE author SET first_name = ?, last_name = ? WHERE id = ?",
+                author.getFirstName(), author.getLastName(), author.getId());
+        return this.getById(author.getId());
     }
 
     @Override
